63c27015bc6142bd8e4115c7983942c6482d35e8
tegarpratama <halloegar@gmail.com>
2021-01-11 23:21:40 +0700

    template engine

---
+++ 4-dynamic-content/app.js
@@ -0,0 +1,23 @@
+const path = require('path');
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+app.set('view engine', 'pug'); // set engine template
+app.set('views', 'views'); // this set default
+
+const adminData = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/admin', adminData.routes);
+app.use(shopRoutes);
+
+app.use((req, res, next) => {
+    res.status(404).render('404');
+});
+
+app.listen(3000);
