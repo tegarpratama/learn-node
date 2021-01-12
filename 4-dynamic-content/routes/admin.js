27bee5a28c495d1ce4a43ce5e090add67fc98024
tegarpratama <halloegar@gmail.com>
2021-01-12 09:39:01 +0700

    finish with pug

--- 4-dynamic-content/routes/admin.js
+++ 4-dynamic-content/routes/admin.js
@@ -5,7 +5,7 @@ const router = express.Router();
 const products = [];
 
 router.get('/add-product', (req, res, next) => {
-  res.render('add-product', {pageTitle: 'Add Prodct'});
+  res.render('add-product', {pageTitle: 'Add Prodct', path: '/admin/add-product'});
 });
 
 router.post('/add-product', (req, res, next) => {
