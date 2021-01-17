954e1aba10a71a0d3e53412169d050989779cc66
tegarpratama <halloegar@gmail.com>
2021-01-17 09:51:32 +0700

    updated little bit of project

--- 9-work-with-sequelize(unfinish)/controllers/admin.js
+++ 9-work-with-sequelize(unfinish)/controllers/admin.js
@@ -13,12 +13,14 @@ exports.postAddProduct = (req, res, next) => {
   const imageUrl = req.body.imageUrl;
   const price = req.body.price;
   const description = req.body.description;
-  Product.create({
+  // createProduct is feature sequelize, where Product is model name was created
+  req.user.createProduct({
     title: title,
     price: price,
     imageUrl: imageUrl,
     description: description
-  }).then(result => {
+  })
+  .then(result => {
     res.redirect('/admin/products');
   }).catch(err => {
     console.log(err);
@@ -31,9 +33,10 @@ exports.getEditProduct = (req, res, next) => {
     return res.redirect('/');
   }
   const prodId = req.params.productId;
-  Product.findByPk(prodId)
-    .then(product => {
-      if (!editMode) {
+  req.user.getProducts({where: {id: prodId}})
+    .then(products => {
+      const product = products[0];
+      if (!product) {
         return res.redirect('/');
       }
       res.render('admin/edit-product', {
@@ -67,7 +70,7 @@ exports.postEditProduct = (req, res, next) => {
 };
 
 exports.getProducts = (req, res, next) => {
-  Product.findAll()
+  req.user.getProducts()
     .then(products => {
       res.render('admin/products', {
         prods: products,
