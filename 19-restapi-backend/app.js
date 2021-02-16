1c2e30c3fd5b113428ff25cfffb1ddcb8dd64d43
tegarpratama <halloegar@gmail.com>
2021-02-16 12:19:02 +0700

    add create-read-edit

--- 19-restapi-backend/app.js
+++ 19-restapi-backend/app.js
@@ -1,12 +1,40 @@
+const path = require('path');
+
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const multer = require('multer');
 
 const feedRoutes = require('./routes/feed');
 
 const app = express();
 
+const fileStorage = multer.diskStorage({
+   destination: (req, file, cb) => {
+      cb(null, 'images');
+   },
+   filename: (req, file, cb) => {
+      cb(null, new Date().getTime() + '-' + file.originalname);
+   }
+});
+
+const fileFilter = (req, file, cb) => {
+   if (
+      file.mimetype === 'image/png' ||
+      file.mimetype === 'image/jpg' ||
+      file.mimetype === 'image/jpeg'
+   ) {
+      cb(null, true);
+   } else {
+      cb(null, false);
+   }
+};
+
 app.use(bodyParser.json());
+app.use(
+   multer({ storage: fileStorage, fileFilter: fileFilter}).single('image')
+);
+app.use('/images', express.static(path.join(__dirname, 'images')));
 
 app.use((req, res, next) => {
    res.setHeader('Access-Control-Allow-Origin', '*');
@@ -16,6 +44,13 @@ app.use((req, res, next) => {
 });
 
 app.use('/feed', feedRoutes);
+
+app.use((error, req, res, next) => {
+   console.log(error);
+   const status = error.statusCode || 500;
+   const message = error.message;
+   res.status(status).json({ message: message });
+});
 
 mongoose.connect(
    'mongodb://Tegar:7hKcyOkJHbfkeMS0@mycluster-shard-00-00.iazfi.mongodb.net:27017,mycluster-shard-00-01.iazfi.mongodb.net:27017,mycluster-shard-00-02.iazfi.mongodb.net:27017/messages?ssl=true&replicaSet=atlas-3wt673-shard-0&authSource=admin&retryWrites=true&w=majority',
--- 19-restapi-backend/routes/feed.js
+++ 19-restapi-backend/routes/feed.js
@@ -19,4 +19,19 @@ router.post(
    ],
    feedController.postPost);
 
+router.get('/post/:postId', feedController.getPost)
+
+router.put(
+   '/post/:postId',
+   [
+      body('title')
+         .trim()
+         .isLength({ min: 5 }),
+      body('content')
+         .trim()
+         .isLength({ min: 5 })
+   ],
+   feedController.updatePost
+);
+
 module.exports = router;
