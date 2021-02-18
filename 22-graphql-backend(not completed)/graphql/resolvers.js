c25aa8057ba7baed69a3d07a01d4254a1919cbae
tegarpratama <halloegar@gmail.com>
2021-02-18 13:31:47 +0700

    skip course graphql

---
+++ 22-graphql-backend(not completed)/graphql/resolvers.js
@@ -0,0 +1,45 @@
+const bcrypt = require('bcryptjs');
+const validator = require('validator');
+
+const User = require('../models/user');
+
+module.exports = {
+  createUser: async function({ userInput }, req) {
+    const errors = [];
+
+    if (!validator.isEmail(userInput.email)
+    ){
+      errors.push({ message: 'E-mail is invalid' });
+    } 
+
+    if (
+      validator.isEmpty(userInput.password) || 
+      !validator.isLength(userInput.password, { min: 5 })) {
+        errors.push({ message: 'Password to short!' });
+    }
+
+    if (existingUser) {
+      const error = new Error('User exists already!');
+      throw error;
+    }
+
+    if (errors.length > 0) {
+      const error = new Error('Invalid input!');
+      throw error;
+    }
+    
+    const existingUser = await User.findOne({ email: userInput.email });
+    if (existingUser) {
+      const error = new Error('User exists already!');
+      throw error;
+    }
+    const hashedPw = await bcrypt.hash(userInput.password, 12);
+    const user = new User({
+      email: userInput.email,
+      name: userInput.name,
+      password: hashedPw
+    });
+    const createdUser = await user.save();
+    return { ...createdUser._doc, _id: createdUser._id.toString() };
+  }
+};
